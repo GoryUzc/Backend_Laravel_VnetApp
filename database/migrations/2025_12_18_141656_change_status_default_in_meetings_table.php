c8079a02278626e50831b1f84d0fcd5a581e98fc
goryUzcategui <gory.2096@gmail.com>
2025-12-18 10:28:07 -0400

    Agg migration: change default status no_asignada to table meetings.

---
+++ database/migrations/2025_12_18_141656_change_status_default_in_meetings_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('meetings', function (Blueprint $table) {
+            
+            $table->string('status')->default('no_asignada')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('meetings', function (Blueprint $table) {
+            
+            $table->string('status')->default('no_asignada')->change();
+        });
+    }
+};
